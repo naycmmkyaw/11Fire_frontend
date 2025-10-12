5d17493b794655ef0b358068adfbfd7636298f6d
Su Lei Yin Win <suleiyinwin@gmail.com>
2025-10-13 04:09:51 +0700

    fix(AuthPage): delete unused baseurl

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -16,7 +16,7 @@ interface AuthPageProps {
 const AuthPage: React.FC<AuthPageProps> = ({ isInitialLoading = false }) => {
   const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
-  const baseUrl = import.meta.env.VITE_BACKEND_API_URL;
+  // const baseUrl = import.meta.env.VITE_BACKEND_API_URL;
 
   const isSignIn = location.pathname === '/auth/signin' || location.pathname === '/';
   const showLoading = isInitialLoading || isLoading;
